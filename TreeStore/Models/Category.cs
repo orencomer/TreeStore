a52b47bf4714a0944b7e2fd37055f50f18b87871
ndrgcgl <gecgelnadir@gmail.com>
2017-05-26 15:55:57 +0300

    category entity ad campany

--- TreeStore/Models/Category.cs
+++ TreeStore/Models/Category.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TreeStore.Models
 {
-    public class Category
+    public class Category:BaseEntity
     {
-        public string name { get; set; }
-        public string description { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long? CampaingId { get; set; }
         public virtual Campaign Campaing { get; set; }
-        public long CampaingId { get; set; }
-        
     }
 }
